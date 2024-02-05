32000701484992a068cafbcf9b048ff433b0b7b1
nishisuke <ryoskns+git@gmail.com>
2024-02-05 16:00:30 +0900

    fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-project/internal/model/task"
+	"golang-project/internal/server"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	dmysql "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func exitIfError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 
 func main() {
 
-	fmt.Println("hi")
+	err := godotenv.Load()
+	exitIfError(err)
 
+	jst, err := time.LoadLocation(os.Getenv("TZ"))
+	exitIfError(err)
+
+	// TODO: user/passなどもENV化
+	db, err := openGORM(os.Getenv("DB_NAME"), "root", "example", "0.0.0.0", os.Getenv("MYSQL_PORT_HOST"), jst)
+	exitIfError(err)
+
+	err = db.AutoMigrate(&task.Task{})
+	exitIfError(err)
+
+	e, err := server.NewServer(db)
+	exitIfError(err)
+
+	// TODO: PORTもENV化
+	e.Logger.Fatal(e.Start(":9000"))
+}
+
+func openGORM(dbName, user, pass, host, port string, loc *time.Location) (*gorm.DB, error) {
+	m := mysql.New(mysql.Config{
+		DSNConfig: &dmysql.Config{
+			User:      user,
+			Passwd:    pass,
+			DBName:    dbName,
+			Addr:      fmt.Sprintf("%s:%s", host, port),
+			Net:       "tcp",
+			Loc:       loc,
+			ParseTime: true,
+		},
+		DontSupportRenameIndex:        true,
+		DontSupportRenameColumn:       true,
+		DontSupportNullAsDefaultValue: true,
+		DontSupportRenameColumnUnique: true,
+	})
+
+	db, err := gorm.Open(m, &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.21.4
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.4
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.6
 )
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
